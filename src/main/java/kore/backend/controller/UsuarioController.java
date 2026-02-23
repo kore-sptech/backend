59ba7301dc72407444e8583ed211e28df1a5a7b6
Henrytos <henry.arcaya@sptech.school>
2026-02-23 19:46:09 -0300

    liberando cors

--- src/main/java/kore/backend/controller/UsuarioController.java
+++ src/main/java/kore/backend/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*") 
 @RequestMapping("/usuarios")
 public class UsuarioController {
     private final UsuarioService usuarioService;
