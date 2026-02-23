e4138faa2a329a269f9f89e004135c77b23e8920
Henrytos <henry.arcaya@sptech.school>
2026-02-23 16:11:23 -0300

    chore: configurando banco de dados h2

--- src/main/java/kore/backend/controller/UsuarioController.java
+++ src/main/java/kore/backend/controller/UsuarioController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/usuarios")
 public class UsuarioController {
     private final UsuarioService usuarioService;
 
