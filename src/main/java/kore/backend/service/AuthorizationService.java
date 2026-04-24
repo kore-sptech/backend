56e0a3a7643f36d0d08fc6fb74e3c2b9dd8b07ab
AnaMioki <ana.mioki@sptech.school>
2026-04-24 14:41:38 -0300

    feat: implementa authorization service para o uso do userdetails, configura o security web personalizado com o metodo do security filter chain

---
+++ src/main/java/kore/backend/service/AuthorizationService.java
@@ -0,0 +1,30 @@
+package kore.backend.service;
+
+import kore.backend.model.Usuario;
+import kore.backend.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(usuario.getEmail())
+                .password(usuario.getSenha())
+                .authorities(Collections.emptyList()) // sem roles
+                .build();
+    }
+}
