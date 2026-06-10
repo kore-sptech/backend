3b5c2d92a6d2db1b28519dfefabf800c6643d727
Henrytos <henry.arcaya@sptech.school>
2026-06-10 00:09:36 -0300

    refactor: improve code formatting and consistency in ItemController

--- src/main/java/kore/backend/controller/ItemController.java
+++ src/main/java/kore/backend/controller/ItemController.java
@@ -12,31 +12,35 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/estoque")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ItemController {
     private final ItemService itemService;
+
     public ItemController(ItemService itemService) {
         this.itemService = itemService;
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<List<Item>> buscarEstoque(@PathVariable Long id){
+    public ResponseEntity<List<Item>> buscarEstoque(@PathVariable Long id) {
         List<Item> estoque = itemService.listarEstoque(id);
         if (estoque.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok().body(estoque);
     }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> removerEstoque(@PathVariable Long id){
+    public ResponseEntity<Void> removerEstoque(@PathVariable Long id) {
         itemService.removerEstoque(id);
         return ResponseEntity.noContent().build();
     }
+
     @PostMapping("/{qtd}/{id}")
     public ResponseEntity<List<Item>> adicionarEstoque(
             @PathVariable Integer qtd,
             @PathVariable Long id,
-            @RequestBody ItemDTO itemDTO
-            ){
-        List<Item> e =  itemService.adicionarEstoque(itemDTO, qtd, id);
+            @RequestBody ItemDTO itemDTO) {
+        List<Item> e = itemService.adicionarEstoque(itemDTO, qtd, id);
         return ResponseEntity.status(HttpStatus.CREATED).body(e);
     }
 }
