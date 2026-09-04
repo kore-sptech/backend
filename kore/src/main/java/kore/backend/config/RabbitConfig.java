d5c989dec4177f8733674f3a8682b6523bbf1be4
Henrytos <henry.arcaya@sptech.school>
2026-09-04 19:51:36 -0300

    refactor: update RabbitMQ configuration and remove unused settings

--- kore/src/main/java/kore/backend/config/RabbitConfig.java
+++ kore/src/main/java/kore/backend/config/RabbitConfig.java
@@ -15,24 +15,35 @@ public class RabbitConfig {
     @Value("${spring.rabbitmq.queue.name}")
     private String QUEUE_NAME;
 
+    static final String topicExchangeName = "spring-boot-exchange";
+
     @Bean
-    public Queue queue() {
-        return new Queue(QUEUE_NAME, true);
+    Queue queue() {
+        return new Queue(QUEUE_NAME, false);
     }
 
     @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
     }
 
     @Bean
-    public SimpleRabbitListenerContainerFactory listenerContainerFactory(
-            ConnectionFactory connectionFactory,
-            Jackson2JsonMessageConverter jsonConverter) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(jsonConverter);
-        return factory;
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
     }
 
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(QUEUE_NAME);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
 }
--- notification/src/main/java/kore/notification/config/RabbitConfig.java
+++ notification/src/main/java/kore/notification/config/RabbitConfig.java
@@ -1,4 +1,4 @@
-package kore.notification.config;
+package notification.backend.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -6,6 +6,8 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 
 @Configuration
 public class RabbitConfig {
@@ -13,14 +15,35 @@ public class RabbitConfig {
     @Value("${spring.rabbitmq.queue.name}")
     private String QUEUE_NAME;
 
+    static final String topicExchangeName = "spring-boot-exchange";
+
     @Bean
     Queue queue() {
-        return new Queue(QUEUE_NAME, true);
+        return new Queue(QUEUE_NAME, false);
     }
 
     @Bean
-    MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
     }
 
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(QUEUE_NAME);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
 }
