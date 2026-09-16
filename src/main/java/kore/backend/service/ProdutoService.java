a06f97a5f42694c54158eaefccf76c3e7de5a530
Henrytos <henry.arcaya@sptech.school>
2026-09-16 20:58:26 -0300

    Refactor: Remove unused ProdutoValidationService and clean up code in ProdutoService

--- src/main/java/kore/backend/service/ProdutoService.java
+++ src/main/java/kore/backend/service/ProdutoService.java
@@ -1,6 +1,5 @@
 package kore.backend.service;
 
-import jakarta.persistence.EntityExistsException;
 import jakarta.transaction.Transactional;
 import kore.backend.dto.produto.ProdutoDTO;
 import kore.backend.exception.RecursoNaoEncontradoException;
@@ -9,7 +8,6 @@ import kore.backend.model.Produto;
 import kore.backend.repository.ProdutoRepository;
 import kore.backend.service.policy.ProdutoPolicy;
 import kore.backend.service.validation.CategoriaValidationService;
-import kore.backend.service.validation.ProdutoValidationService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
@@ -20,19 +18,15 @@ import java.util.List;
 public class ProdutoService {
     private final ProdutoRepository produtoRepository;
     private final CategoriaValidationService categoriaValidationService;
-    private final ProdutoValidationService produtoValidationService;
     private final ProdutoPolicy produtoPolicy;
     private final S3StorageService s3StorageService;
 
     public ProdutoService(ProdutoRepository produtoRepository,
             CategoriaValidationService categoriaValidationService,
-            ProdutoValidationService produtoValidationService,
             ProdutoPolicy produtoPolicy,
-            S3StorageService s3StorageService
-        ) {
+            S3StorageService s3StorageService) {
         this.produtoRepository = produtoRepository;
         this.categoriaValidationService = categoriaValidationService;
-        this.produtoValidationService = produtoValidationService;
         this.produtoPolicy = produtoPolicy;
         this.s3StorageService = s3StorageService;
     }
@@ -46,24 +40,19 @@ public class ProdutoService {
         return produtoRepository.save(p);
     }
 
-    
     @Transactional
     public Produto salvarImagem(Long produtoId, MultipartFile arquivo) {
 
         Produto produto = produtoRepository.findById(produtoId)
-                .orElseThrow(() ->
-                        new RecursoNaoEncontradoException(
-                                "Produto nao encontrado",
-                                produtoId
-                        )
-                );
+                .orElseThrow(() -> new RecursoNaoEncontradoException(
+                        "Produto nao encontrado",
+                        produtoId));
 
         try {
 
             String objectKey = s3StorageService.generateObjectKey(
                     produtoId,
-                    arquivo.getOriginalFilename()
-            );
+                    arquivo.getOriginalFilename());
 
             s3StorageService.upload(objectKey, arquivo);
 
@@ -82,11 +71,9 @@ public class ProdutoService {
         } catch (IOException e) {
             throw new RuntimeException(
                     "Erro ao enviar imagem para o S3",
-                    e
-            );
+                    e);
         }
     }
-
 
     public List<Produto> listarTodosProdutos(Long fkUsuario) {
         produtoPolicy.validarUsuario(fkUsuario);
@@ -110,4 +97,3 @@ public class ProdutoService {
         produtoRepository.delete(p);
     }
 }
-
